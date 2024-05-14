97377a85cab3e8eca83c9efd98f9923b90835127
Natalie Mbatha <natalie.mbatha@protofy.com>
2024-05-14 13:43:28 +0200

    feat: create and return a record

--- lib/models/data.dart
+++ lib/models/data.dart
@@ -4,6 +4,22 @@ class Document {
   /// define and initialize JSON data
   final Map<String, Object?> _json;
   Document() : _json = jsonDecode(documentJson);
+
+  /// create a getter that returns a record
+  (String, {DateTime modified}) get metadata {
+    const title = 'My Document';
+    final now = DateTime.now();
+
+    /// this return type is a record with two fields
+    /// the first field is a string, the second field is a DateTime
+    /// the return statement constructs a new record by enclosing
+    /// the two values in parentheses, the first field is positional
+    /// & unnamed, the second field is named
+    return (
+      title,
+      modified: now,
+    );
+  }
 }
 
 /// mocking incoming JSON data
