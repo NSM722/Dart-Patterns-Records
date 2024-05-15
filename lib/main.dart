083a2c46812184909791da08a66c098c699c11c2
Natalie Mbatha <natalie.mbatha@protofy.com>
2024-05-15 10:31:52 +0200

    refactor: improve how the last modified date is displayed

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,23 @@ void main() {
   runApp(const DocumentApp());
 }
 
+/// It represents the span of time between
+/// today and the modified value from the JSON data
+String formatDate(DateTime dateTime) {
+  final today = DateTime.now();
+  final difference = dateTime.difference(today);
+
+  return switch (difference) {
+    /// object pattern matching on the Duration class
+    /// for the inDays and isNegative properties
+    Duration(inDays: 0) => 'today',
+    Duration(inDays: 1) => 'tomorrow',
+    Duration(inDays: -1) => 'yesterday',
+    Duration(inDays: final days, isNegative: true) => '${days.abs()} days ago',
+    Duration(inDays: final days) => '$days days from now',
+  };
+}
+
 class DocumentApp extends StatelessWidget {
   const DocumentApp({super.key});
 
